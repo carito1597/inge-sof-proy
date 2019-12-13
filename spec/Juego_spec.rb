d904a5efe423b2281388c318a6c0527a8fa6980f
carito <carito15villalobos@gmail.com>
2019-12-13 09:08:02 -0400

    b

--- spec/Juego_spec.rb
+++ spec/Juego_spec.rb
@@ -1,13 +1,12 @@
-require './app.rb'
 
-prueba=Prueba.new
+# prueba=Prueba.new
 
 
-RSpec.describe Prueba do
-    it 'Deberia devolver 7*8' do
+# RSpec.describe Prueba do
+#     it 'Deberia devolver 7*8' do
         
-        prueba.crearTerreno(7,8)
-        expect(prueba.terreno()).to eq("7,8")
-    end
+#         prueba.crearTerreno(7,8)
+#         expect(prueba.terreno()).to eq("7,8")
+#     end
     
-end
+# end
